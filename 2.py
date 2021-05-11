9e3c3d4e3affef8f424e49f94c061e6c6009b73f
Usernam-kimyoonha <82634058+Usernam-kimyoonha@users.noreply.github.com>
2021-05-11 20:57:04 +0900

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,8 +1,3 @@
-x=5
-y=3
-z=1.2
-print("1)", x + y +z)
-print("3)", x*y*z)
-print("4)", x/y/z)
-print("5)", x**y)
-print("6)", x%y)
+x = "Hello" # 변수 x에 문자 Hello 선언
+y = 'Bye' # 변수 y에 문자 Bye 선언
+z = """안녕하세요. 저는 21학번 새내기입니다""" # 변수 z에 문자 2행 선언
